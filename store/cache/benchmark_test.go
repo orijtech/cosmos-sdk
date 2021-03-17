f72e419516858fbeb81f53c90393c23095a0fa4f
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-03-16 18:36:30 -0700

    store/cache: produce CommitKVStore benchmark
    Experimenting with denoising benchmarks.

--- store/cache/benchmark_test.go
+++ store/cache/benchmark_test.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/store/mem"
 	"github.com/cosmos/cosmos-sdk/store/types"
 )
 
@@ -45,5 +46,42 @@ func BenchmarkReset(b *testing.B) {
 
 	if mgr == nil {
 		b.Fatal("Impossible condition")
+	}
+}
+
+func BenchmarkCommitKVStore(b *testing.B) {
+	b.ReportAllocs()
+	kvL := []struct {
+		key, value []byte
+	}{
+		{[]byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), []byte("value")},
+		{[]byte("value"), []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")},
+		{[]byte("nil"), []byte("nil")},
+		{[]byte("0"), []byte("1")},
+		{[]byte("0"), []byte("nothing")},
+		{[]byte("nothing"), []byte("0")},
+		{[]byte{0x01, 0x12, 0x22, 0x77, 0x44, 0x7f, 0x1c}, []byte("0")},
+		{[]byte("0"), []byte{0x01, 0x12, 0x22, 0x77, 0x44, 0x7f, 0x1c}},
+		{[]byte{0x10, 0x11}, []byte{0x01, 0x12, 0x22, 0x77, 0x44, 0x7f, 0x1c}},
+		{[]byte{0x10, 0x11}, []byte{0x11, 0x22, 0x22, 0x77, 0x44, 0xff, 0xcc}},
+		{[]byte{0xff, 0x11}, []byte{0x11, 0x22, 0x22, 0x77, 0x44, 0xff, 0xcc}},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ms := mem.NewStore()
+		ckv := NewCommitKVStoreCache(ms, 10)
+		for _, kv := range kvL {
+			ckv.Set(kv.key, kv.value)
+		}
+		for _, kv := range kvL {
+			ckv.Get(kv.key)
+			ckv.Get(kv.value)
+		}
+		for _, kv := range kvL {
+			ckv.Delete(kv.key)
+		}
+		for _, kv := range kvL {
+			ckv.Delete(kv.value)
+		}
 	}
 }
